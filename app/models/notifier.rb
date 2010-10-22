9182d929624c820481d287fde71f32c922eb5995
Julien Raigneau <julien@zeneffy.fr>
2010-10-22 15:25:08 +0200

    Added @content-type for email

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,18 +2,19 @@ class Notifier < ActionMailer::Base
  default_url_options[:host] = "vauban.heroku.com"  
  
  def password_reset_instructions(user)
-    subject      "Password Reset Instructions for Vauban"
-    from         "vauban@zeneffy.fr"
-    recipients   user.email
-    content_type "text/html"
-    sent_on      Time.now
-    body         :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
+    @subject      = "Password Reset Instructions for Vauban"
+    @from         = "vauban@zeneffy.fr"
+    @recipients   = user.email
+    @content_type = "text/html"
+    @sent_on      = Time.now
+    @body[:edit_password_reset_url] = edit_password_reset_url(user.perishable_token)
   end
 
  def feedback(feedback)
     @recipients  = 'vauban@zeneffy.fr'
     @from        = 'vauban@zeneffy.fr'
     @subject     = "[Feedback for Vauban.zeneffy.fr] #{feedback.subject}"
+    @content_type="text/html"
     @sent_on     = Time.now
     @body[:feedback] = feedback    
   end 
@@ -22,6 +23,7 @@ class Notifier < ActionMailer::Base
     @recipients  = 'vauban@zeneffy.fr'
     @from        = 'vauban@zeneffy.fr'
     @subject     = "[Vauban.zeneffy.fr] New user!"
+    @content_type="text/html"
     @sent_on     = Time.now
     @body[:user] = user
   end
@@ -30,6 +32,7 @@ class Notifier < ActionMailer::Base
     @recipients  = user.email
     @from        = 'vauban@zeneffy.fr'
     @subject     = "Connection to your Vauban account"
+    @content_type="text/html"
     @sent_on     = Time.now
     @body[:user] = user
   end
